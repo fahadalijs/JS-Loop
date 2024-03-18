c804d599d6e6a4be1ff77fa77bdad934518154cf
fahadalijs <fahad9827022@gmail.com>
2024-03-18 11:38:59 +0500

    JS-loop

---
+++ app.js
@@ -0,0 +1,76 @@
+// console.log(Hello loop)
+
+
+// for (let i = 1 ; i <= 10; i++){
+//    console.log(i);
+//    for (let j = 1; j <= 10; j++){
+//       console.log(`${i}*${j} = ${i*j}`);
+//  }
+// }
+
+
+
+
+
+
+
+// for (let i = 1; i <= 100; i++) {
+   // console.log();
+   // for (let j = 1; j <= 10; j++) {
+      // console.log(`${i} * ${j} = ${i * j}`);
+   // }
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//    console.log(i);
+//    for (let j = 1; j <= 10; j++) {
+//       console.log(`${i} * ${j} = ${i * j}`)
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+// let fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'grapes'];
+// console.log(fruits);
+// const ul = document.querySelector("ul");
+
+// for (let i = 0; i < fruits.length; i++){
+//    // console.log(fruits[i]);
+//    ul.innerHTML += `<li>${fruits[i]}</li>`
+
+// }
+
+
+
+
+// let fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'grapes']
+//    console.log(fruits);
+//    const ul = document.querySelector("ul");
+
+
+//    for (let i = 0; i < fruits.length; i++) {
+//       ul.innerHTML += `<li>${fruits[i]}</li>`
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
