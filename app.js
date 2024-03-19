1c4f6b0f458d489f18d983bb5875f4d473260b68
fahadalijs <fahad9827022@gmail.com>
2024-03-19 13:02:45 +0500

    file modified

--- app.js
+++ app.js
@@ -74,3 +74,193 @@
 
 
 
+
+// const fruits = [
+//     "apple",
+//     "banana",
+//     "orange",
+//     "grape",
+//     "strawberry",
+//     "kiwi",
+//     "watermelon",
+//     "pineapple",
+//     "mango",
+//     "pear",
+//     "peach",
+//     "plum",
+//     "blueberry",
+//     "raspberry",
+//     "cherry",
+//     "lemon",
+//     "lime",
+//     "apricot",
+//     "pomegranate",
+//     "coconut"
+// ];
+// const fruits = ["apple" , "Bannana" , "Mango" , "oring" , "Graps"];
+// console.log(fruits);
+// const ol = document.querySelector("ol")
+// function renderfruits(){
+    // for (let i = 0; i < fruits.length; i++) {
+        // console.log(fruits[i]);
+        // ol.innerHTML += `<li>${fruits[i]}</li>`
+    // }
+// }
+
+
+ 
+
+
+
+// const fruits = [
+//    "apple",
+//    "banana",
+//    "orange",
+//    "grape",
+//    "strawberry",
+//    "kiwi",
+//    "watermelon",
+//    "pineapple",
+//    "mango",
+//    "pear",
+//    "peach",
+//    "plum",
+//    "blueberry",
+//    "raspberry",
+//    "cherry",
+//    "lemon",
+//    "lime",
+//    "apricot",
+//    "pomegranate",
+//    "coconut"
+// ];
+// console.log(fruits);
+// const ol = document.querySelector("ol")
+// function renderFruits(){
+//     for (let i = 0; i < fruits.length; i++) {
+//         console.log(fruits[i]);
+//         ol.innerHTML += `<li>${fruits[i]}</li>`
+//     }
+// }
+
+
+
+
+
+// const fruits = [
+//    "apple",
+//    "banana",
+//    "orange",
+//    "grape",
+//    "strawberry",
+//    "kiwi",
+//    "watermelon",
+//    "pineapple",
+//    "mango",
+//    "pear",
+//    "peach",
+//    "plum",
+//    "blueberry",
+//    "raspberry",
+//    "cherry",
+//    "lemon",
+//    "lime",
+//    "apricot",
+//    "pomegranate",
+//    "coconut"
+// ];
+// console.log(fruits);
+// const ol = document.querySelector("ol")
+// function renderFruits(){
+//    for (let i = 0; i < fruits.length; i++) {
+//       console.log(fruits[i]);
+//       ol.innerHTML += `<li>${fruits[i]}</li>`
+//    }
+// }
+
+
+
+
+
+
+
+
+// const fruits = [
+//    "apple",
+//    "banana",
+//    "orange",
+//    "grape",
+//    "strawberry",
+//    "kiwi",
+//    "watermelon",
+//    "pineapple",
+//    "mango",
+//    "pear",
+//    "peach",
+//    "plum",
+//    "blueberry",
+//    "raspberry",
+//    "cherry",
+//    "lemon",
+//    "lime",
+//    "apricot",
+//    "pomegranate",
+//    "coconut"
+// ];
+//  console.log(fruits);
+//  const ol = document.querySelector("ol");
+//  function renderFruits(){
+//    for (let i = 0; i < fruits.length; i++) {
+//       console.log(fruits[i]);
+//       ol.innerHTML += `<li>${fruits[i]}</li>`
+//    }
+
+//  } 
+
+
+
+
+
+
+
+//infinite loops
+
+// let i = 1;
+// while(i > 0){
+//     console.log(i);
+//     i++
+// }
+
+
+// while loop
+// let j = 11;
+// while (j < 10){
+//    console.log("while loop " , j);
+//    j++
+// }
+
+
+
+
+//do while loop
+// let i = 11;
+// do {
+//     console.log('do while loop ===>', i)
+//     i++
+// } while (i < 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
